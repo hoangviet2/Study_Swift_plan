642bddd5e1cdd4fc626a194b3ca47c00ea6fa599
Viet Hoang <hoangviet18306@gmail.com>
2019-04-15 13:32:14 +0700

    Sixth commit

---
+++ AutoLayuot/AutoLayuot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayuot
+//
+//  Created by Hoang Viet on 4/13/19.
+//  Copyright © 2019 Hoang Viet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Navigation & Lifecycle/Navigation & Lifecycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navigation & Lifecycle
+//
+//  Created by Hoang Viet on 4/14/19.
+//  Copyright © 2019 Hoang Viet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
