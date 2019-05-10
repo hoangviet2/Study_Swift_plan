ea56d0e1291cc9cdbd64b828ba9f1017269e5727
Viet Hoang <hoangviet18306@gmail.com>
2019-05-10 19:13:28 +0700

    30 comiits

--- Frist App/Frist App/ViewController.swift
+++ Frist App/Frist App/ViewController.swift
@@ -9,72 +9,89 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-    @IBOutlet weak var txtend: UITextField!
+    @IBOutlet weak var lblExchange: UILabel!
     @IBOutlet weak var pickunit: UIPickerView!
     @IBOutlet weak var pickunitend: UIPickerView!
     @IBOutlet weak var txtEntrain: UITextField!
-    var toolbar = UIToolbar()
-//    var donebuttom = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action:)
+    var so1:Int!
+    var so2:Int = 100
+    var data:Int!
     let arrUnitStart = ["milimet","centimet","deximet","met","decamet","hectamet","kilomet"]
+    let arrUnitEnd = ["milimet","centimet","deximet","met","decamet","hectamet","kilomet"]
     override func viewDidLoad() {
         pickunit.delegate = self
         pickunit.dataSource = self
         pickunitend.dataSource = self
         pickunitend.delegate = self
         txtEntrain.delegate = self
-        
-        
-//        var occupations:[String:Any] = [
-//            "Malcolm": "Captain",
-//            "Kaylee": "Mechanic",
-//        ]
-//        occupations["Jayne"] = "Public Relations"
-//        occupations["cut"] = 1
-//        print(occupations)
-        txtEntrain.returnKeyType = .next
     }
+    
     func numberOfComponents(in pickerview: UIPickerView) -> Int {
         return 1
     }
+    
     func pickerView(_ pickerview: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return arrUnitStart.count
+        let pickerview2 = pickerview
+        if pickerview2 == pickunit as! UIPickerView{
+            return arrUnitStart.count
+        }
+        if pickerview2 == pickunitend as! UIPickerView{
+            return arrUnitEnd.count
+        }
+        else{
+            return arrUnitStart.count
+        }
     }
+    
     func pickerView(_ pickerview: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return arrUnitStart[row]
+        let pickerview1 = pickerview
+        if  pickerview1 == pickunit as! UIPickerView {
+            return arrUnitStart[row]
+            
+        }
+        if pickerview1 == pickunitend as! UIPickerView {
+            return arrUnitEnd[row]
+        }
+        else{
+            return arrUnitStart[row]
+        }
     }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let pickerview3 = pickerView
+        let pickerview4 = pickerView
+        if pickerview3 == pickunit {
+            if component == 0{
+                if pickerview4 == pickunitend{
+                    switch (component){
+                    case 0:
+                        data == 0
+                    default:
+                        print("Erro")
+                    }
+                }
+            }
+        }
+    }
+    
     @IBAction func btnExchange(_ sender: Any) {
-        
-        
+        let s1 = txtEntrain.text!
+        so1 = Int(s1)
+        var kq:Int = 0
+        kq = so1 * so2
+        lblExchange.text = String(kq)
+        if data == 0{
+          print("Success")
+        }
     }
 }
 extension ViewController:UITextFieldDelegate{
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        print("textFieldDidEndEditing")
-    }
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        print("textFieldDidBeginEditing")
-    }
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        print("textFieldShouldEndEditing")
-        
-        return true
-    }
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        print("textFieldShouldBeginEditing")
-        return true
-    }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         print("textFieldShouldReturn")
         textField.resignFirstResponder()
-//        if textField.tag == 1 {
-//
-//        }
-//        if textField == self.txtEntrain {
-//
-//        }
-        
         return (true)
     }
+    
     
 
 }
